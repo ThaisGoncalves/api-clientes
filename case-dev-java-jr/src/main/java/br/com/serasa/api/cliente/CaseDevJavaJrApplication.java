9738ec8b1e33ebae12147fcb0a5e4c9920281088
ThaisGoncalves <thais.gosil@gmail.com>
2021-10-07 22:51:26 -0300

    post, get, swagger

---
+++ case-dev-java-jr/src/main/java/br/com/serasa/api/cliente/CaseDevJavaJrApplication.java
@@ -0,0 +1,13 @@
+package br.com.serasa.api.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseDevJavaJrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseDevJavaJrApplication.class, args);
+	}
+
+}
